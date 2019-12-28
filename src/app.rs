8c83b469e782048b5be96a526d9983ade1892eeb
MainShayne233 <shaynetremblay@gmail.com>
2019-12-28 16:56:03 -0500

    Get keypress working!

--- src/app.rs
+++ src/app.rs
@@ -1,26 +1,42 @@
 use log::*;
 use serde_derive::{Deserialize, Serialize};
-use strum::IntoEnumIterator;
-use strum_macros::{EnumIter, ToString};
-use yew::events::IKeyboardEvent;
-use yew::format::Json;
-use yew::services::storage::{Area, StorageService};
-use yew::{html, Component, ComponentLink, Href, Html, Renderable, ShouldRender};
+use yew::services::Task;
+use yew::{html, Callback, Component, ComponentLink, Html, Renderable, ShouldRender};
 
-const KEY: &'static str = "yew.turtris.self";
+mod keydown_service;
+use keydown_service::KeydownService;
+
+use stdweb::traits::IKeyboardEvent;
+use stdweb::web::event::KeyDownEvent;
 
 const BOARD_WIDTH: usize = 10;
 const BOARD_HEIGHT: usize = 24;
 
 pub struct App {
-    storage: StorageService,
     state: State,
+    keydown_service: KeydownService,
+    keydown_cb: Callback<KeyDownEvent>,
+    keydown_job: Option<Box<Task>>,
 }
 
 #[derive(Serialize, Deserialize, Clone)]
 struct Piece {
     color: Color,
     position: (usize, usize, usize, usize),
+}
+
+impl Piece {
+    pub fn new() -> Self {
+        Piece {
+            color: Color::Yellow,
+            position: (4, 5, 14, 15),
+        }
+    }
+
+    pub fn occupies_cell(&self, index: usize) -> bool {
+        let (w, x, z, y) = self.position;
+        index == w || index == x || index == z || index == y
+    }
 }
 
 #[derive(Serialize, Deserialize, Clone)]
@@ -34,6 +50,20 @@ enum Color {
     Red,
 }
 
+impl Color {
+    fn to_hex(&self) -> String {
+        match self {
+            Color::Turquoise => String::from("#40e0d0"),
+            Color::Blue => String::from("#4169e1"),
+            Color::Orange => String::from("#ffa500"),
+            Color::Yellow => String::from("#ffff00"),
+            Color::Green => String::from(" #00ff00"),
+            Color::Purple => String::from("#800080"),
+            Color::Red => String::from("#ff0000"),
+        }
+    }
+}
+
 #[derive(Serialize, Deserialize, Clone)]
 struct Cell {
     color: Option<Color>,
@@ -43,10 +73,6 @@ type Board = Vec<Cell>;
 
 #[derive(Serialize, Deserialize)]
 pub struct State {
-    entries: Vec<Entry>,
-    filter: Filter,
-    value: String,
-    edit_value: String,
     board: Board,
     current_piece: Option<Piece>,
 }
@@ -59,6 +85,8 @@ struct Entry {
 }
 
 pub enum Msg {
+    ListenKeydown,
+    HandleKeyDown(KeyDownEvent),
     Nope,
 }
 
@@ -66,31 +94,33 @@ impl Component for App {
     type Message = Msg;
     type Properties = ();
 
-    fn create(_: Self::Properties, _: ComponentLink<Self>) -> Self {
-        let storage = StorageService::new(Area::Local);
-        let entries = {
-            if let Json(Ok(restored_entries)) = storage.restore(KEY) {
-                restored_entries
-            } else {
-                Vec::new()
-            }
-        };
+    fn create(_: Self::Properties, mut link: ComponentLink<Self>) -> Self {
         let state = State {
-            entries,
-            filter: Filter::All,
-            value: "".into(),
-            edit_value: "".into(),
             board: init_board(),
-            current_piece: Some(init_piece()),
+            current_piece: Some(Piece::new()),
         };
-        App { storage, state }
+        let app = App {
+            state,
+            keydown_service: KeydownService::new(),
+            keydown_cb: link.send_back(|e| Msg::HandleKeyDown(e)),
+            keydown_job: None,
+        };
+        link.send_self(Msg::ListenKeydown);
+        app
     }
 
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        info!("Update!");
         match msg {
-            Msg::Nope => {}
+            Msg::ListenKeydown => {
+                let handle = self.keydown_service.spawn(self.keydown_cb.clone());
+                self.keydown_job = Some(Box::new(handle));
+            },
+            Msg::HandleKeyDown(e) => {
+                info!("on key down from rust!");
+            }
+            _ => {}
         }
-        self.storage.store(KEY, Json(&self.state.entries));
         true
     }
 }
@@ -99,20 +129,13 @@ fn init_board() -> Board {
     vec![Cell { color: None }; BOARD_WIDTH * BOARD_HEIGHT]
 }
 
-fn init_piece() -> Piece {
-    Piece {
-        color: Color::Yellow,
-        position: (4, 5, 14, 15),
-    }
-}
-
 impl Renderable<App> for App {
     fn view(&self) -> Html<Self> {
         info!("rendered!");
         html! {
-            <div class="app">
-              { view_state( &self.state) }
-            </div>
+           <div class="app">
+             { view_state( &self.state) }
+           </div>
         }
     }
 }
@@ -125,14 +148,9 @@ fn view_state(state: &State) -> Html<App> {
     }
 }
 
-fn piece_has_index(piece: &Piece, index: usize) -> bool {
-    let (w, x, z, y) = piece.position;
-    index == w || index == x || index == z || index == y
-}
-
 fn view_cell((index, cell): (usize, &Cell), current_piece: Option<&Piece>) -> Html<App> {
     let color = match current_piece {
-        Some(piece) if piece_has_index(piece, index) => color_to_hex(&piece.color),
+        Some(piece) if piece.occupies_cell(index) => piece.color.to_hex(),
         _ => cell_color(cell),
     };
 
@@ -145,37 +163,8 @@ fn view_cell((index, cell): (usize, &Cell), current_piece: Option<&Piece>) -> Ht
 
 fn cell_color(cell: &Cell) -> String {
     match &cell.color {
-        Some(color) => color_to_hex(color),
+        Some(color) => color.to_hex(),
         _ => String::from("white"),
-    }
-}
-
-fn color_to_hex(color: &Color) -> String {
-    match color {
-        Color::Turquoise => String::from("#40e0d0"),
-        Color::Blue => String::from("#4169e1"),
-        Color::Orange => String::from("#ffa500"),
-        Color::Yellow => String::from("#ffff00"),
-        Color::Green => String::from(" #00ff00"),
-        Color::Purple => String::from("#800080"),
-        Color::Red => String::from("#ff0000"),
-    }
-}
-
-#[derive(EnumIter, ToString, Clone, PartialEq, Serialize, Deserialize)]
-pub enum Filter {
-    All,
-    Active,
-    Completed,
-}
-
-impl<'a> Into<Href> for &'a Filter {
-    fn into(self) -> Href {
-        match *self {
-            Filter::All => "#/".into(),
-            Filter::Active => "#/active".into(),
-            Filter::Completed => "#/completed".into(),
-        }
     }
 }
 
